0490253c7ba74ce6ee0a395f7b23f00004be92d4
Michael Evans <michael.evans@umbragestudios.com>
2020-01-15 16:16:49 -0600

    installed firebase
    Attempt 2: integrating firebase
    
    removed old firebase setup
    
    fixed spelling and auth_domain key

---
+++ ostore-frontend/set-env.ts
@@ -0,0 +1,72 @@
+import { writeFile } from 'fs';
+
+require('dotenv').load();
+const colors = require('colors');
+
+const targetPath = './src/environments/environment.ts';
+const targetProdPath = './src/environments/environment.prod.ts';
+
+const envConfigFile = `export const environment = {
+   production: false,
+   apiKey: '${process.env.API_KEY}',
+   authDomain: '${process.env.AUTH_DOMAIN}',
+   databaseURL: '${process.env.DATABASE_URL}',
+   projectId: '${process.env.PROJECT_ID}',
+   storageBucket: '${process.env.STORAGE_BUCKET}',
+   messagingSenderId: '${process.env.MESSAGING_SENDER_ID}',
+   appId: '${process.env.APP_ID}',
+   measurementId: '${process.env.MEASUREMENT_ID}' 
+};
+`;
+
+const envConfigProdFile = `export const environment = {
+  production: true,
+  apiKey: '${process.env.API_KEY}',
+  authDomain: '${process.env.AUTH_DOMAIN}',
+  databaseURL: '${process.env.DATABASE_URL}',
+  projectID: '${process.env.PROJECT_ID}',
+  storageBucket: '${process.env.STORAGE_BUCKET}',
+  messaginSenderId: '${process.env.MESSAGING_SENDER_ID}',
+  appId: '${process.env.APP_ID}',
+  measurementId: '${process.env.MEASUREMENT_ID}' 
+};
+`;
+
+console.log(
+  colors.magenta(
+    'The file `environment.ts` will be written with the following content: \n'
+  )
+);
+
+console.log(colors.grey(envConfigFile));
+
+writeFile(targetPath, envConfigFile, function(err) {
+  if (err) {
+    throw console.error(err);
+  } else {
+    console.log(
+      colors.magenta(
+        `Angular environment.ts file generated correctly at ${targetPath} \n`
+      )
+    );
+  }
+});
+
+console.log(
+  colors.magenta(
+    'The file `environment.prod.ts` will be written with the following content: \n'
+  )
+);
+
+console.log(colors.grey(envConfigProdFile));
+writeFile(targetProdPath, envConfigProdFile, function(err) {
+  if (err) {
+    throw console.error(err);
+  } else {
+    console.log(
+      colors.magenta(
+        `Angular environment.prod.ts file generated correctly at ${targetProdPath} \n`
+      )
+    );
+  }
+});
